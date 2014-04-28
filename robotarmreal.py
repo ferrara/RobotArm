df319fa5cc6d5aefc9744b0fc35bea3b48991448
KingNg <king.ng@live.ca>
2014-04-28 03:55:06 -0400

    visuals
    added visuals and animation to arm

--- robotarmreal.py
+++ robotarmreal.py
@@ -9,11 +9,38 @@ class seg:
     def __init__(self, length, angle):
         self.l = length
         self.a = np.radians(angle) ## this is the actual angle, not relative. stored internally in rads
+
+
+fig = plt.figure("CS 4TE3 Robot Simulator")
+ax = plt.axes([0.05, 0.2, 0.90, .75],xlim=200,ylim=200)
+#ax.axis([0,200,0,200])
+plt.ion()
+
+temp = plt.axes([0.0, 0.0, 0.01, 0.01])
+#but = Button(butval, '')
+#but.on_clicked(move_click)
+
+
+#plt.show()
+
         
 basePos = [0,0] ## the static initial position (start point of first segment)
 goalPos = [100, 100]
 segments = [seg(80, 60), seg(100,60), seg(60, 60)]
 nSegs = len(segments)
+
+
+
+def draw(x,y):
+    ax.cla() #clear current axis
+    ax.plot(x,y,'o-', markersize=20, markerfacecolor="red", linewidth = 8, color="black")
+    ax.plot(x,y,'o-', markersize=4, markerfacecolor="blue", linewidth = 1, color="silver")
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    #ax.relim()
+    ax.autoscale_view()
+    plt.draw()
+    
         
 
     # returns the segment's end point position in world coords. Ask for
@@ -57,18 +84,20 @@ def absDist(a, b):
     # add this angle to your current angle (use sine law to
     # determine if angle is positive or negative)
 def ccd():
+    xs = [None]*4
+    ys = [None]*4
     goalRadi = 10 # how close to the goal we need to get
     currentPos= getHeadPos() # current position of the tip
-    print "curr Pos: ", currentPos
-    print "goal Pos: ", goalPos
+    print ("curr Pos: ", currentPos)
+    print ("goal Pos: ", goalPos)
     iter = 0
     while(absDist(currentPos, goalPos) > goalRadi): # keep trying until close enough (or error)
-        print "iterations: ", iter
+        print ("iterations: ", iter)
         for i in reversed(range(nSegs)): #adjust each segment, starting at the tip
 
             currentPos= getHeadPos() # current position of the tip
             segPos = getSegPos(i) # position of the base of the current segment
-            print "segment ", i+1, " is at ", segPos
+            print ("segment ", i+1, " is at ", segPos)
             
             l2c = dist(currentPos, segPos) # line segment from current tip position to segment base position
             l2g = dist(goalPos, segPos) # line segment from goal position to segment base position
@@ -85,16 +114,22 @@ def ccd():
             
             dAng = sign_dAng * magn_dAng
             segments[i].a += sign_dAng * magn_dAng #update angle of the segment
+            xs[i] = segPos[0]
+            ys[i] = segPos[1]
+            xs[3] = currentPos[0]
+            ys[3] = currentPos[1]
             
+        draw(xs,ys)
+        time.sleep(2)
         iter+=1
         
-        print "current arm head position: ", getHeadPos()
+        print ("current arm head position: ", getHeadPos())
         
         if (iter > 100):
-            print "Error: could not reach point"
+            print ("Error: could not reach point")
             break
         
-    print "position ", currentPos, " within accepted range of goal"
+    print ("position ", currentPos, " within accepted range of goal")
     return "lol"
 
 def main():
@@ -104,7 +139,7 @@ def main():
     if (totalArmLength * 0.9 > absDist(goalPos, basePos)):
         ccd()
     else:
-        print "Error: goal point too far away"
+        print ("Error: goal point too far away")
         
 if __name__ == '__main__':
     main()
